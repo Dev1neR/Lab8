f251d26ef8ab1f0e8aa874bf8a8c8f9b0835e943
Dev1neR <35506504+Dev1neR@users.noreply.github.com>
2018-11-29 17:19:12 +0200

    Added solution

---
+++ Lab8Library/Processor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8Library
+{
+    public class Processor
+    {
+        private string family;
+        public string Family
+        {
+            get { return family; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    throw new InvalidFamilyException();
+                }
+                else
+                {
+                    family = value;
+                }
+            }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (value.Length > 5)
+                {
+                    throw new InvalidModelException();
+                }
+                else
+                {
+                    model = value;
+                }
+            }
+        }
+
+        private string socket;
+        public string Socket
+        {
+            get { return socket; }
+            set
+            {
+                if (value.Length > 6)
+                {
+                    throw new InvalidSocketException();
+                }
+                else
+                {
+                    socket = value;
+                }
+            }
+        }
+
+        private double cores;
+        public double Cores
+        {
+            get { return cores; }
+            set
+            {
+                if (value > 16)
+                {
+                    throw new InvalidCoresException();
+                }
+                else
+                {
+                    cores = value;
+                }
+            }
+        }
+
+        private int freq;
+        public int Freq
+        {
+            get { return freq; }
+            set
+            {
+                if (value > 0 && value < 5000)
+                {
+                    freq = value;
+                }
+                else
+                    throw new InvalidFreqException();
+            }
+        }
+
+        public bool HasGraphics { get; set; }
+        public bool HasMultiplier { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} | Socket: {2} | Ядер: {3} | Частота: {4} MHz| [{5}, {6}]\r\n",
+                Family, Model, Socket,
+                Cores, Freq,
+                HasMultiplier ? "Є помножувач" : "Немає помножувача",
+                HasGraphics ? "Є інтегрована карта" : "Немає інтегрованої карти"); ;
+        }
+    }
+}
---
+++ Lab8Library/ProcessorException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8Library
+{
+    class ProcessorException : Exception
+    {
+        public ProcessorException(string message) : base(message) { }
+    }
+
+    class InvalidFamilyException : ProcessorException
+    {
+        public InvalidFamilyException() : base("Type correct family") { }
+    }
+
+    class InvalidModelException : ProcessorException
+    {
+        public InvalidModelException() : base("Invalid model") { }
+    }
+
+    class InvalidFreqException : ProcessorException
+    {
+        public InvalidFreqException() : base("Processor with this frequency does not exist") { }
+    }
+
+    class InvalidSocketException : ProcessorException
+    {
+        public InvalidSocketException() : base("Invalid Socket") { }
+    }
+
+    class InvalidCoresException : ProcessorException
+    {
+        public InvalidCoresException() : base("Invalid number of cores") { }
+    }
+}
