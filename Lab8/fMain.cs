f99a9f3d12f23d912957d0c1b946b3e341233ec1
Dev1neR <35506504+Dev1neR@users.noreply.github.com>
2018-11-30 20:38:30 +0200

    Corrected binding

--- Lab8/fMain.cs
+++ Lab8/fMain.cs
@@ -13,20 +13,21 @@ namespace Lab8
 {
     public partial class fMain : Form
     {
-
         public fMain()
         {
             InitializeComponent();
-
             List<Processor> _processors = new List<Processor>
             {
                 new Processor {Id = 1, Family = "i5", Model = "4790", Cores = 8, Socket = "1151", Freq = 3200, HasMultiplier = true, HasGraphics = false},
                 new Processor {Id = 2, Family = "i7", Model = "4690", Cores = 4, Socket = "1151", Freq = 3600, HasMultiplier = true, HasGraphics = true},
             };
 
+            lbProcessorInfo.DisplayMember = "Model";
+            lbProcessorInfo.ValueMember = "Id";
             lbProcessorInfo.DataSource = _processors;
-            //lbProcessorInfo.DisplayMember = "Model";
-            //lbProcessorInfo.ValueMember = "Id";
+
+
+            lbProcessorInfo.SelectedIndexChanged += new EventHandler(lbProcessorInfo_SelectedIndexChanged);
         }
 
 
@@ -49,6 +50,20 @@ namespace Lab8
             {
                 Application.Exit();
             }
+        }
+
+        private void lbProcessorInfo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = (int)lbProcessorInfo.SelectedValue;
+
+            Processor processor = (Processor)lbProcessorInfo.SelectedItem;
+            MessageBox.Show(id.ToString() + ". " + "Процесор: " + processor.Family + " " +
+                processor.Model + " | Ядер: " +
+                processor.Cores + " | Тип роз'єму: " +
+                processor.Socket + " | Частота: " +
+                processor.Freq + "MHz | Помножувач: " +
+                processor.HasMultiplier + " | Наявність відеокарти: " +
+                processor.HasGraphics, "Інформація о процесорі");
         }
     }
 }
